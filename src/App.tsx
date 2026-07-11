23a1305cf5e3ec32eb5dafd2011bae0e5a94c744
ram9096 <ashaysudharsan2000@gmail.com>
2026-07-11 18:23:31 +0530

    Fix: build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 import AXLandingView from './views/AXLandingView';
 import AboutPage from './views/AboutPage';
 import { FounderSummitView } from './views/summit/FounderSummitView';
@@ -29,8 +27,6 @@ function App() {
         <Route path="/founders" element={<FoundersPage />} />
         <Route path="*" element={<NotFoundView />} />
       </Routes>
-      <Analytics />
-      <SpeedInsights />
     </BrowserRouter>
   );
 }
